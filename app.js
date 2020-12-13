ea492ab0362dadde7c4655e7fdcb077d3c204e52
swag31415 <kidswagatam18@gmail.com>
2020-12-13 13:41:43 -0500

    Added cards to the main page

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
   res.render("index", {
-    nav_pages: [{name: "Example", link: "https://google.com"}]
+    nav_pages: [{name: "Example", link: "https://google.com"}],
+    cards: [{
+      title: "Example",
+      desc: "this is an example card",
+      page_link: "https://google.com",
+      github_link: "https://github.com",
+      image_link: "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
+    }]
   })
 })
 
