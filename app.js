fb6d3e01c790c61537bb6036ef679791b276c33d
swag31415 <kidswagatam18@gmail.com>
2020-12-15 10:27:47 -0500

    Added the coderart

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const app = express()
   .set("view engine", "ejs")
+  .use(express.static('public'))
 const port = process.env.PORT || 3000
+
+const nav_pages = [{name: "Coder Art", link: "coderart"}]
 
 app.get("/", (req, res) => {
   res.render("index", {
-    nav_pages: [{name: "Example", link: "https://google.com"}],
+    nav_pages: nav_pages,
     cards: [{
       title: "Example",
       desc: "this is an example card",
@@ -13,6 +16,12 @@ app.get("/", (req, res) => {
       github_link: "https://github.com",
       image_link: "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
     }]
+  })
+})
+
+app.get("/coderart", (req, res) => {
+  res.render("projects/coderart", {
+    nav_pages: nav_pages
   })
 })
 
