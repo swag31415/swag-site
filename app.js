e3da82fdb051dd2936b6088392006a6546354e12
swag31415 <kidswagatam18@gmail.com>
2020-12-25 19:53:23 -0500

    Added Txty

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.get("/", (req, res) => {
       page_link: "jugs",
       github_link: "https://github.com/swag31415/Jugs",
       image_link: "/media/jug.png"
+    }, {
+      title: "Txty",
+      desc: "A no-frills Quill-based in-browser text editor with hotkeys for everything! I find it suprisingly useful to dump information and gather my thoughts",
+      page_link: "https://swag31415.github.io/Txty/",
+      github_link: "https://github.com/swag31415/Txty",
+      image_link: "/media/txty.png"
     }]
   })
 })
