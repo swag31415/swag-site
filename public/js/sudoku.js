a867820ff18665036f09fd082832b3b2ff0db22c
swag31415 <kidswagatam18@gmail.com>
2021-02-10 16:06:24 -0500

    Maybe? made things faster by reducing jquery

--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -1,18 +1,27 @@
 const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const grps = ["r", "c", "b"]
 const rand = (arr) => arr[Math.floor(Math.random() * arr.length)]
-let data = {}
+let data = {}, tiles = {}
 
-$("td.tile").each((i, t) => data[t.id] = [])
+$("td.tile").each((i, t) => {
+  data[t.id] = [[],[...nums]]
+  grps.forEach((g, i) => {
+    grp = g + t.id[i]
+    if (grp in tiles) tiles[grp].push(t)
+    else tiles[grp] = [t]
+  })
+})
 
 let move_stack = []
 function update(tile, val) {
   let old = tile.innerText
   if (val == old) return;
-  move_stack.push([tile, old])
-  tile.className.match(/[rcb]\d/g).forEach(g => $("td.tile." + g).each((i, t) => {
-    blklst = data[t.id]
+  move_stack.push([tile, old]);
+  grps.forEach((g, i) => tiles[g + tile.id[i]].forEach(t => {
+    blklst = data[t.id][0]
     if (old != "") blklst.splice(blklst.indexOf(old), 1)
     if (val != "") blklst.push(val)
+    data[t.id][1] = nums.filter(n => !blklst.includes(n))
   }))
   tile.innerText = val
 }
@@ -28,7 +37,7 @@ function do_implicit() {
   while (imp) {
     imp = false
     $("td.tile:empty").each((i, t) => {
-      posi = nums.filter(n => !data[t.id].includes(n))
+      posi = data[t.id][1]
       if (posi.length == 1) {
         update(t, posi[0])
         imp = true
@@ -40,30 +49,28 @@ function do_implicit() {
 function solve() {
   do_implicit()
   let init = move_stack.length
-  while ($("td.tile:empty").length > 0) {
+  for (h = 0; h < 20; h++) {
     revert(init)
-    for (let i = 0; (i < 65) && ($("td.tile:empty").length > 0); i++) {
-      mat = [...$("td.tile:not(.grey)")]
-        .map(t => [t, nums.filter(n => !data[t.id].includes(n))])
-        .filter(v => v[1].length != 0)
-      let [t, posi] = rand(mat)
-      update(t, rand(posi))
+    for (let i = 0; i < 65; i++) {
+      t = rand([...$("td.tile:not(.grey,.green)")].filter(t => data[t.id][1].length != 0))
+      update(t, rand(data[t.id][1]))
       do_implicit()
+      if ($("td.tile:empty").length == 0) return true;
     }
   }
+  return false
 }
 
 function reset() {
-  $("td.tile").removeClass("grey lighten-2").each((i, t) => update(t, ""))
+  $("td.tile").removeClass("grey green lighten-2").each((i, t) => update(t, ""))
   move_stack = []
 }
 
 function generate(n = 70) {
-  reset()
-  update(rand($("td.tile")), rand(nums))
-  solve()
+  reset(); solve()
+  let pool = [...$("td.tile")]
   for(i = 0; i < n; i++) {
-    update(rand($("td.tile")), "")
+    update(rand(pool), "")
   }
   $("td.tile:not(:empty)").addClass("grey lighten-2")
 }
@@ -72,14 +79,14 @@ function generate(n = 70) {
 var hovered;
 $("td.tile").hover(e => hovered = e.target, e => hovered = undefined)
 $(document).keyup(e => {
-  if (hovered && nums.includes(e.key) && !data[hovered.id].includes(e.key)) {
+  if (hovered && data[hovered.id][1].includes(e.key)) {
     update(hovered, e.key)
-    $(hovered).addClass("grey lighten-2")
+    $(hovered).addClass("green lighten-2")
   } else if (hovered && e.which == 46) {
     update(hovered, "")
-    $(hovered).removeClass("grey lighten-2")
+    $(hovered).removeClass("green grey lighten-2")
   }
 })
-$("#solve").click(() => solve())
+$("#solve").click(() => {if (!solve()) alert("thats impossible")})
 $("#generate").click(() => generate())
 $("#clear").click(reset)
