54df9461c60d6c7d25654bd6f92598008ab0ecc6
swag31415 <kidswagatam18@gmail.com>
2021-05-16 22:49:52 -0400

    Switched to daily data and svgs for rendering

--- public/js/stonkey.js
+++ public/js/stonkey.js
@@ -3,31 +3,48 @@ async function get_data(symbol) {
   data = await resp.text()
   if (data.length < 300) throw data // If it's really short something's up
   return data
-    .split('\n')
-    .slice(1,-1)
-    .map(row => parseFloat(row.split(',')[1]))
-    .reverse()
+  .split('\n')
+  .slice(1,-1)
+  .reverse()
+  .reduce((dict, row) => {
+    [date, val, ...rest] = row.split(',')
+    date = date.substring(0, 10)
+    val = parseFloat(val)
+    if (date in dict) dict[date].push(val)
+    else dict[date] = [val];
+    return dict
+  }, {})
 }
 
-const can = document.getElementById("bana")
-const ctx = can.getContext("2d")
-function plot(arr) {
+const line = document.getElementById("line")
+const [width, height] = [300, 100]
+
+async function play(arr, step) {
+  // Normalize values
   [min, max] = [Math.min(...arr), Math.max(...arr)]
-  arr = arr.map(v => can.height - ((v - min) * (can.height / (max - min))))
-  ctx.clearRect(0, 0, can.width, can.height)
-  ctx.beginPath()
-  ctx.moveTo(0, arr.splice(0, 1))
-  arr.forEach((v, i) => {
-    x = (i + 1) * (can.width / arr.length)
-    ctx.lineTo(x, v)
-  })
-  ctx.stroke()
+  arr = arr.map(v => height - ((v - min) * (height / (max - min))))
+  // Clear
+  points = ""
+  line.setAttribute("points", points)
+  // Render
+  for (let i = 0; i < arr.length; i++) {
+    x = (i + 1) * (width / arr.length)
+    // Add the point
+    points += `${x} ${arr[i]} `
+    line.setAttribute("points", points)
+    // Animation
+    await new Promise(r => setTimeout(r, step))
+  }
 }
 
-$("#play").click(() => {
-  get_data($("#sym").val())
-  .then(plot).catch(err => {
-    console.log(err)
+$("#play").click(async () => {
+  // Get the data
+  try {
+    queue = await get_data($("#sym").val())
+  } catch (err) {
+    console.error(err)
     alert("there was an error")
-  })
+    return;
+  }
+  play(Object.values(queue)[0], 50)
 })
