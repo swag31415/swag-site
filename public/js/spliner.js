6d19f7b567c203090b0749e529bd7222239902d3
swag31415 <kidswagatam18@gmail.com>
2021-08-12 11:04:49 -0400

    Added drawing with splines

--- public/js/spliner.js
+++ public/js/spliner.js
@@ -4,13 +4,17 @@ paper.setup("disp")
 const tool = new Tool()
 var cur_path
 
+tool.onKeyUp = e => {
+  if (e.key == "s") {
+    cur_path = new Path()
+    cur_path.strokeColor = "white"
+  }
+}
+
 tool.onMouseDown = e => {
-  cur_path = new Path()
-  cur_path.strokeColor = "white"
   cur_path.add(e.point)
 }
 
-tool.onMouseDrag = e => {
-  cur_path.add(e.point)
+tool.onMouseUp = e => {
+  cur_path.smooth()
 }
-view.draw()
