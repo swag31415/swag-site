c6b426b9fb8fdf9e5b775b14dd4bbf1aebc11487
swag31415 <kidswagatam18@gmail.com>
2021-08-12 17:25:44 -0400

    Implemented an actual draw 'tool'

--- public/js/spliner.js
+++ public/js/spliner.js
@@ -1,20 +1,30 @@
 paper.install(window)
 paper.setup("disp")
 
-const tool = new Tool()
-var cur_path
+var cur_path, shad_path
 
-tool.onKeyUp = e => {
-  if (e.key == "s") {
-    cur_path = new Path()
-    cur_path.strokeColor = "white"
+const main_tool = new Tool()
+const draw_tool = new Tool()
+
+main_tool.onMouseDown = e => {
+  // Need two for the onMouseMove stuff
+  cur_path = new Path([e.point, e.point])
+  cur_path.strokeColor = "white"
+  draw_tool.activate()
+}
+
+draw_tool.onKeyUp = e => {
+  if (e.key == "escape") {
+    cur_path.lastSegment.remove()
+    main_tool.activate()
   }
 }
 
-tool.onMouseDown = e => {
+draw_tool.onMouseDown = e => {
   cur_path.add(e.point)
 }
 
-tool.onMouseUp = e => {
-  cur_path.smooth()
+draw_tool.onMouseMove = e => {
+  cur_path.lastSegment.point = e.point
+  cur_path.smooth({type: 'continuous'})
 }
