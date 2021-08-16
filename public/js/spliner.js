31ed0c9f05fbb6df4fe39859bfec96a360d70cac
swag31415 <kidswagatam18@gmail.com>
2021-08-15 22:06:06 -0400

    Can edit and add points

--- public/js/spliner.js
+++ public/js/spliner.js
@@ -50,3 +50,34 @@ edit_tool.onKeyUp = e => {
     M.toast({html: "<span>Switched back to <strong>Draw</strong> mode</span>"})
   }
 }
+
+const hit_opts = {
+  segments: true,
+  stroke: true,
+  tolerance: 5
+}
+var target_path = null
+var target_seg = null
+edit_tool.onMouseDown = e => {
+  let hit = project.hitTest(e.point, hit_opts)
+  if (hit) {
+		target_path = hit.item
+		if (hit.type == "stroke") {
+			target_seg = target_path.insert(hit.location.index + 1, e.point)
+			target_path.smooth()
+		} else {
+      target_seg = hit.segment
+    }
+  }
+}
+
+edit_tool.onMouseDrag = e => {
+  if (!target_path || !target_seg) return;
+  target_seg.point = e.point
+  target_path.smooth({type:"continuous"})
+}
+
+edit_tool.onMouseUp = e => {
+  target_path = null
+  target_seg = null
+}
